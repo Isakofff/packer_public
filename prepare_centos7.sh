e1bf030c83e1d1e4f2e654d36afe22809dbda5c7
Anton Isakov <ping115115@gmail.com>
2020-08-04 23:20:03 +0700

    refactoring

--- prepare_centos7.sh
+++ prepare_centos7.sh
@@ -1,9 +1,10 @@
-echo -e "\n[+] Install epel-release repo\n"
+echo -e "\n[+] Install required repositories\n"
 sudo yum install -y epel-release
-echo -e "\n[+] Install Docker stable repo\n"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 echo -e "\n[+] Update system\n"
 sudo yum update -y
+
 echo -e "\n[+] Install required packages\n"
 sudo yum install -y \
     yum-utils \
@@ -29,7 +30,12 @@ sudo yum install -y \
     git \
     python3 \
     python3-pip
+# for Ansible
+sudo pip3 install docker
+
 echo -e "\n[+] Enable docker daemon\n"
 sudo systemctl enable docker
+
 echo -e "\n[+] Add 'centos' user to 'docker' group\n"
 sudo usermod -aG docker centos
+
