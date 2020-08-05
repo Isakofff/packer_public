534ce170327ecc51edbe6faaadee430b5f12d26f
Anton Isakov <ping115115@gmail.com>
2020-08-05 14:19:07 +0700

    add Centos7 server hardening

---
+++ centos7_hardening/Readme.md
@@ -0,0 +1,5 @@
+```
+ansible-galaxy install -r requirements.yml
+ll ~/.ansible/roles
+packer build -var-file ../variables.json centos.json
+```
---
+++ centos7_hardening/prepare_centos7.sh
@@ -0,0 +1,39 @@
+echo -e "\n[+] Install required repositories\n"
+sudo yum install -y epel-release
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo -e "\n[+] Update system\n"
+sudo yum update -y
+
+echo -e "\n[+] Install required packages\n"
+sudo yum install -y \
+    yum-utils \
+    jq \
+    docker-ce \
+    wget \
+    mc \
+    whois \
+    htop \
+    net-tools \
+    iotop \
+    which \
+    unzip \
+    bind-utils \
+    traceroute \
+    tree \
+    file \
+    vim-enhanced \
+    ncdu \
+    screen \
+    git \
+    python3 \
+    python3-pip
+# for Ansible
+sudo pip3 install docker
+
+echo -e "\n[+] Enable docker daemon\n"
+sudo systemctl enable docker
+
+echo -e "\n[+] Add 'centos' user to 'docker' group\n"
+sudo usermod -aG docker centos
+
